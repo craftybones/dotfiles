42786887e5e28c55d6244b613803e142c2b77ff9
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2019-10-02 21:58:49 +0530

    Updated cider and eastwood

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.21.0-SNAPSHOT"]
                   [lein-kibit "0.1.2"]
                   [mies/lein-template "0.6.12"]
                   [lein-cljfmt "0.5.3"]
-                  [jonase/eastwood "0.2.3"]]}}
+                  [jonase/eastwood "0.3.5"]]}}
 
