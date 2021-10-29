804679f9f3c22055fef774ca54fedeacf76c228c
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2021-10-29 10:52:08 +0530

    REmoved vim

--- install_vim_plugins.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-for i in $(cat plugin_repos); do
-  plugin_name=$(echo ${i} | sed 's/.*\/\([^/]*\)\.git/\1/')
-  git submodule add ${i} vim/pack/plugins/start/${plugin_name}
-done
